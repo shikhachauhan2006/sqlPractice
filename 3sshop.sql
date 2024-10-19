89386d24adc5300e28ac406ff87186f06daf1d4f
Khushbu <dishu2281@gmail.com>
2024-10-19 14:12:02 +0530

    create queries for order tables

--- 3sshop.sql
+++ 3sshop.sql
@@ -1,1 +1,5 @@
- create database 3sshop;
+create database 3sshop;
+
+create table orders(order_id int,customer_id int);
+create table order_details(product_quantity int,shipping_address varchar(255));
+create table payment_details(order_status varchar(50),total_amount decimal(10, 2));

9850a850ac42f9a7bd04ce19c2ebc96617e9c094
shikha <chauhanshikha701@gmail.com>
2024-10-19 14:00:01 -0700

    create queries for product tables

--- 3sshop.sql
+++ 3sshop.sql
@@ -1,1 +1,5 @@
- create database 3sshop;
+create database 3sshop;
+
+create table product(product_id int,product_name varchar(24));
+create table product_detail(product_id int,product_quantity int,product_price int);
+create table product_offer(product_id int,discount_status varchar(21),discount_percent varchar(22),product_amount int);

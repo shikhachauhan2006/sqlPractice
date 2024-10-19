28866bb73486334d1fd88f36ed852a644bd443cc
Lalitkumar2004-cloud <lk7474592@gmail.com>
2024-10-19 13:42:55 +0530

    create query for user table

--- 3sshop.sql
+++ 3sshop.sql
@@ -1,1 +1,8 @@
- create database 3sshop;
+create database 3sshop;
+
+create table user (id int(12) primary key, name varchar(15) NOT NULL, email varchar(25) NOT NULL, phonenumber int(10) NOT NULL ) ;
+
+create table user_detail (id int(12), name varchar(15) NOT NULL, fathername varchar(18) NOT NULL, mothername varchar(16) NOT NULL, phonenumber int(10
+) NOT NULL ) ;
+
+create table user_address (currentaddress varchar(100), permanentaddress varchar(100) ) ;

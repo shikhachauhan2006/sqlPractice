1f752ae969fae7ddefcd0341e63c71f15c757d3c
Lalitkumar2004-cloud <lk7474592@gmail.com>
2024-10-21 13:27:57 +0530

    user insert values

--- 3sshop.sql
+++ 3sshop.sql
@@ -10,6 +10,22 @@ create table product_detail(product_id int,product_quantity int,product_price in
 create table product_offer(product_id int,discount_status varchar(21),discount_percent varchar(22),product_amount int);
 
 create table user (id int(12) primary key, name varchar(15) NOT NULL, email varchar(25) NOT NULL, phonenumber int(10) NOT NULL ) ;
+  1. insert into user
+     values (1232, 'Anil thakur', 'anu@gamil.com', 1234512345 );
+  2. insert into user
+     values(1233, 'shalu', 'shalu@gamial.com', 1234546789) ;
+  3. insert into user (id, name, email, phonenumber) values(1234, 'shilpa', 'shi@gamail.com', 1234561234);
+
 create table user_detail (id int(12), name varchar(15) NOT NULL, fathername varchar(18) NOT NULL, mothername varchar(16) NOT NULL, phonenumber int(10) NOT NULL ) ;
+  1. insert into user_detail (id, name, fathername, mothername, phonenumber) values(1232, 'Anil thakur', 'dole ram', '
+meena devi', 1234512345) ;
+  2. insert into user_detail (id, name, fathername, mothername, phonenumber) values(1233, 'shalu', 'manipal', 'sbadra devi', 123456789) ;
+  3. insert into user_detail (id, name, fathername, mothername, phonenumber) values(1233, 'shilpa', 'mahesh thakur','durga devi', 1234561234) ;
+
+   
 create table user_address (currentaddress varchar(100), permanentaddress varchar(100) ) ;
+    1. insert into user_address(currentaddress, permanentaddress) values('vill kandhi po dhnotu tehsil balichowki pin 17
+5106', 'vill kandhi po dhnotu tehsil balichowki pin 175106' ) ;
+   2. insert into user_address(currentaddress, permanentaddress) values('vill kulah po baggi tehsil chachyot pin 175006
+', 'vill kulah po baggi tehsil chachyot pin 175006' ) ;
 
